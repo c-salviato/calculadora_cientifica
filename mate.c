78eada7f40a995f80fd5645a794fd5892f36e064
Caique Souza Salviato <caique.s.salviato@gmail.com>
2025-11-28 19:13:17 -0300

    Criando a "mate.h" e algumas funções básicas temporárias

---
+++ mate.c
@@ -0,0 +1,14 @@
+#include "mate.h"
+
+float soma(float a, float b) {
+    return a + b;
+}
+float diferenca(float a, float b) {
+    return a - b;
+}
+float produto(float a, float b) {
+    return a * b;
+}
+float quociente(float a, float b) {
+    return a / b;
+}
