bc2f319583ff04d8084f1686124757b7f27ed0c8
AlissonBecker07 <alisson.borba.becker.07@gmail.com>
2025-12-07 23:44:00 -0300

    Adicionando função de Fatorial

--- CalculadoraCientifica/mate.c
+++ CalculadoraCientifica/mate.c
@@ -1,4 +1,5 @@
 #include "mate.h"
+#include <stdio.h>
 
 float somar(float a, float b) {
     return a + b;
@@ -11,4 +12,23 @@ float multiplicar(float a, float b) {
 }
 float dividir(float a, float b) {
     return a / b;
+}
+float fatorial(float n){
+
+
+
+    if (n < 0 || n != (int)n) {
+        printf("Erro: o fatorial funciona só com inteiros positivos ou zero.\n");
+
+    }
+
+    int num = (int)n;
+    double resultado = 1;
+
+
+    for (int i = 1; i <= num; i++) {
+        resultado = resultado * i;
+    }
+
+    return resultado;
 }
